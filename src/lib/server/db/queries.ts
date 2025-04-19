a0011b179ad52731d989fc18b9109610862d900f
Nemanja Lakicevic <nocontact>
2025-04-19 18:44:31 +0200

    feat: cleanup db queries

--- src/lib/server/db/queries.ts
+++ src/lib/server/db/queries.ts
@@ -1,5 +1,5 @@
 import { DbInternalError, type DbError } from '$lib/errors/db';
-import type { DocumentData, WithFieldValue } from 'firebase-admin/firestore';
+import type { DocumentData, DocumentReference, WithFieldValue } from 'firebase-admin/firestore';
 import { fromPromise, ok, ResultAsync, safeTry } from 'neverthrow';
 import { db } from '../firebase/firebase.admin';
 import type { UserInvitationRecord, UserRecord } from './model';
@@ -11,27 +11,34 @@ enum DbCollection {
 }
 
 function storeRecord<T extends WithFieldValue<DocumentData>>(collection: DbCollection) {
-	return function (record: T): ResultAsync<string, DbError> {
-		return safeTry(async function* () {
+	return (record: T) =>
+		fromPromise(db.collection(collection).add(record), (e) => new DbInternalError({ cause: e }));
+}
+
+function deleteRecordByParam(collection: DbCollection) {
+	return (param: { key: string; value: string }) =>
+		safeTry(async function* () {
 			const result = yield* fromPromise(
-				db.collection(collection).add(record),
+				db.collection(collection).where(param.key, '==', param.value).get(),
 				(e) => new DbInternalError({ cause: e })
 			);
 
-			return ok(result.id);
+			const doc = yield* unwrapSingleQueryResult(result.docs, param.value, collection);
+
+			const deleteResult = yield* fromPromise(
+				doc.ref.delete(),
+				(e) => new DbInternalError({ cause: e })
+			);
+			return ok(deleteResult);
 		});
-	};
 }
 
 function deleteRecordById(collection: DbCollection) {
-	return function (id: string) {
-		return safeTry(async function* () {
+	return (id: string) =>
+		safeTry(async function* () {
 			const result = yield* fromPromise(
 				db.collection(collection).where('id', '==', id).get(),
-				(e) => {
-					console.log(e);
-					return new DbInternalError({ cause: e });
-				}
+				(e) => new DbInternalError({ cause: e })
 			);
 
 			const doc = yield* unwrapSingleQueryResult(result.docs, id, collection);
@@ -42,12 +49,11 @@ function deleteRecordById(collection: DbCollection) {
 			);
 			return ok(deleteResult);
 		});
-	};
 }
 
 function fetchRecordByParamMatch<T>(collection: DbCollection) {
-	return function (param: { key: string; value: unknown }): ResultAsync<T, DbError> {
-		return safeTry(async function* () {
+	return (param: { key: string; value: unknown }): ResultAsync<T, DbError> =>
+		safeTry(async function* () {
 			const result = yield* fromPromise(
 				db.collection(collection).where(param.key, '==', param.value).get(),
 				(e) => new DbInternalError({ cause: e })
@@ -61,8 +67,10 @@ function fetchRecordByParamMatch<T>(collection: DbCollection) {
 
 			return ok(doc.data() as T);
 		});
-	};
 }
+
+export const deleteRecordByRef = (record: DocumentReference) =>
+	fromPromise(record.delete(), (e) => new DbInternalError({ cause: e }));
 
 export const storeInvitationRecord = storeRecord<UserInvitationRecord>(DbCollection.INVITATION);
 export const deleteInvitationById = deleteRecordById(DbCollection.INVITATION);
@@ -70,3 +78,6 @@ export const fetchInvitationByParam = fetchRecordByParamMatch<UserInvitationReco
 	DbCollection.INVITATION
 );
 export const fetchRegisteredUserByParam = fetchRecordByParamMatch<UserRecord>(DbCollection.USER);
+export const storeUserRecord = storeRecord<UserRecord>(DbCollection.USER);
+
+export const deleteInvitationByParam = deleteRecordByParam(DbCollection.INVITATION);
