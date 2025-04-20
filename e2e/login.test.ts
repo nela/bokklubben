20c8ea65b80b67c88cdacea5a6a4418f603b4431
Nemanja Lakicevic <nocontact>
2025-04-20 19:53:55 +0200

    feat: rm playwright tests from pipeline

--- e2e/login.test.ts
+++ e2e/login.test.ts
@@ -1,16 +1,26 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Auth and Login', () => {
+test.describe('Auth and Signin', () => {
+	const userAgent =
+		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36';
+
+	test.use({ userAgent });
+
 	test.describe('User redirection', () => {
-		test('redirects user to login page when unauthenticated', async ({ page }) => {
-			await page.goto('http://localhost:5173/');
-			await expect(page).toHaveURL('http://localhost:5173/login');
+		test('redirects user to signin page when unauthenticated', async ({ page }) => {
+			await page.goto('/');
+			await expect(page).toHaveURL('/signin');
 		});
 
-		test('returns user to login page after failed login', async ({ page }) => {
-			await page.goto('http://localhost:5173/login');
+		test('redirects user from signup to signin page when no seed', async ({ page }) => {
+			await page.goto('/signup');
+			await expect(page).toHaveURL('/signin');
+		});
+
+		test('returns user to signin page after failed signin', async ({ page }) => {
+			await page.goto('/signin');
 			const button = page.getByTestId('email-pass-btn');
-			expect(button).toBeDisabled();
+			// expect(button).toBeDisabled();
 
 			const email = page.getByTestId('email-input');
 			await email.fill('invalid@email.com');
@@ -19,36 +29,30 @@ test.describe('Auth and Login', () => {
 			password.fill('invalid123');
 			await button.click();
 
-			await expect(page).toHaveURL('http://localhost:5173/login');
+			await expect(page).toHaveURL('/signin');
 		});
 
-		test('redirects user to home page on successful login', async ({ page, context }) => {
-			await page.goto('http://localhost:5173/login');
+		test.only('redirects user to home page on successful signin', async ({ page, context }) => {
+			await page.goto('/signin');
 			const button = page.getByTestId('email-pass-btn');
-			expect(button).toBeDisabled();
+			// expect(button).toBeDisabled();
 
 			const email = page.getByTestId('email-input');
 			await email.fill('n@n.no');
 
 			const password = page.getByTestId('password-input');
-			password.fill('123456');
+			await password.fill('123456');
 			await button.click();
-
-			await Promise.all([
-				// page.waitForRequest(/http:\/\/localhost:8080\/.+/),
-				page.waitForResponse(/http:\/\/localhost:9099\/.+signInWithPassword.+/),
-				page.waitForResponse('/login'),
-				page.waitForURL('http://localhost:5173/')
-			]);
+			await page.waitForURL('/');
 
 			const sessionCookie = (await context.cookies()).find((c) => c.name === 'session');
 			expect(sessionCookie).toBeTruthy();
 
-			await expect(page).toHaveURL('http://localhost:5173/');
+			await expect(page).toHaveURL('/');
 		});
 
-		test.skip('rejects login from unregistered users', async ({ page }) => {
-			await page.goto('http://localhost:5173/login');
+		test.skip('rejects signin from unregistered users', async ({ page }) => {
+			await page.goto('/signin');
 			const popupPromise = page.waitForEvent('popup');
 			await page.getByTestId('google-auth-btn').click();
 
@@ -59,19 +63,19 @@ test.describe('Auth and Login', () => {
 
 	test.describe('Admin access', () => {
 		test('regular users cannot access admin routes', async ({ page }) => {
-			await page.goto('http://localhost:5173/login');
+			await page.goto('/signin');
 			await page.getByTestId('password-input').fill('123456');
 			await page.getByTestId('email-input').fill('pleb@n.no');
 			await page.getByTestId('email-pass-btn').click();
 			await page.waitForURL('http://localhost:5173');
 
-			await page.goto('http://localhost:5173/invite');
+			await page.goto('/invite');
 
-			await expect(page).toHaveURL('http://localhost:5173/');
+			await expect(page).toHaveURL('/');
 		});
 
 		test('admin users can access admin routes', async ({ page }) => {
-			await page.goto('/login');
+			await page.goto('/signin');
 			await page.getByTestId('password-input').fill('123456');
 			await page.getByTestId('email-input').fill('n@n.no');
 			await page.getByTestId('email-pass-btn').click();
